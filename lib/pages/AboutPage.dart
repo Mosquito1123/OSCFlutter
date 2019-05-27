58da92faf85f5b1eb7bbd0e2b39349c6f6bf5778
Winston Zhang <winston.zhangwentong@gmail.com>
2019-05-28 01:18:16 +0800

    https://github.com/Mosquito1123/OSCFlutter.git

--- lib/pages/AboutPage.dart
+++ lib/pages/AboutPage.dart
@@ -20,7 +20,7 @@ class AboutPageState extends State<AboutPage> {
   List<String> titles = List();
 
   AboutPageState() {
-    titles.add("yubo's blog");
+    titles.add("Winston");
     titles.add("码云");
     titles.add("GitHub");
     urls.add("https://yubo725.top");
@@ -118,36 +118,36 @@ class AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("关于", style: TextStyle(color: Colors.white)),
-        iconTheme: IconThemeData(color: Colors.white),
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Container(
-              width: 1.0,
-              height: 100.0,
-              color: Colors.transparent,
-            ),
-            Image.asset(
-              './images/ic_osc_logo.png',
-              width: 200.0,
-              height: 56.0,
-            ),
-            Text("基于Google Flutter的开源中国客户端"),
-            authorLink,
-            maYunLink,
-            githubLink,
-            Expanded(flex: 1, child: getImageOrBtn()),
-            Container(
-                margin: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 15.0),
-                child: Text(
-                  "本项目仅供学习使用，与开源中国官方无关",
-                  style: TextStyle(fontSize: 12.0),
-                ))
-          ],
+        appBar: AppBar(
+          title: Text("关于", style: TextStyle(color: Colors.white)),
+          iconTheme: IconThemeData(color: Colors.white),
         ),
-      ));
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: 1.0,
+                height: 100.0,
+                color: Colors.transparent,
+              ),
+              Image.asset(
+                './images/ic_osc_logo.png',
+                width: 200.0,
+                height: 56.0,
+              ),
+              Text("基于Google Flutter的开源中国客户端"),
+              authorLink,
+              maYunLink,
+              githubLink,
+              Expanded(flex: 1, child: getImageOrBtn()),
+              Container(
+                  margin: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 15.0),
+                  child: Text(
+                    "本项目仅供学习使用，与开源中国官方无关",
+                    style: TextStyle(fontSize: 12.0),
+                  ))
+            ],
+          ),
+        ));
   }
 }
